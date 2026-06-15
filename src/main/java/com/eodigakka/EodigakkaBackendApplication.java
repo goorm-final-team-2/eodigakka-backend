30124fb8f556dd947ac9d90bbd31c26a8ba42703
전성우 <81775633+zhdkffk8210@users.noreply.github.com>
2026-06-15 20:58:00 +0900

    chore: 백엔드 프로젝트 초기 설정

---
+++ src/main/java/com/eodigakka/EodigakkaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eodigakka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EodigakkaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EodigakkaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eodigakka/EodigakkaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eodigakka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EodigakkaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
